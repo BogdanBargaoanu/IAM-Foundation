88acc1cf791629b1bcfb9a41162c38a54090fe54
Bogdan Bargaoanu Alexandru <bn19bba@yahoo.com>
2025-12-30 13:25:39 +0200

    feat: implemented registration page and logic

---
+++ Identity/Pages/Account/Register/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Pages.Account.Register
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string? ConfirmPassword { get; set; }
+
+        public string? ReturnUrl { get; set; }
+        public string? Button { get; set; }
+    }
+}
---
+++ Identity/Pages/Account/Register/RegisterOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.Pages.Account.Register
+{
+    public static class RegisterOptions
+    {
+        public static readonly string InvalidPasswordErrorMessage = "You password needs to have 8 or more characters, containing at least a special character and an uppercase.";
+        public static readonly string InvalidUsernameErrorMessage = "The user already exists.";
+        public static readonly string InvalidEmailErrorMessage = "The provided email already exists.";
+    }
+}
