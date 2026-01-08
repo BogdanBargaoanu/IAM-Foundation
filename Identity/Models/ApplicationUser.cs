c9646c2616998243e245a8024d4eddc88f176495
Bogdan Bargaoanu Alexandru <bn19bba@yahoo.com>
2026-01-08 11:22:30 +0200

    feat: extended user class
    Added EmployeeId and PinHash and a mockup seeding user for the kiosk extension grant use.

--- Identity/Models/ApplicationUser.cs
+++ Identity/Models/ApplicationUser.cs
@@ -5,4 +5,6 @@ namespace Identity.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string? EmployeeId { get; set; }
+    public string? PinHash { get; set; }
 }
