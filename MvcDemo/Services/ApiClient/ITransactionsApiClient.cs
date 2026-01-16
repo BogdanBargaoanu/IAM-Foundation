f3347aa26029bed3c1ed56d627a2f211bfd9ada4
Bogdan Bargaoanu Alexandru <bn19bba@yahoo.com>
2026-01-16 09:37:06 +0200

    feat: implemented transactions api client to the MvcDemo

---
+++ MvcDemo/Services/ApiClient/ITransactionsApiClient.cs
@@ -0,0 +1,17 @@
+﻿using TransactionsLibrary.Constants;
+using TransactionsLibrary.Models;
+
+namespace MvcDemo.Services.ApiClient
+{
+    public interface ITransactionsApiClient
+    {
+        Task<bool> CheckHealthy();
+        Task<IReadOnlyList<Transaction>> GetTransactionsAsync(
+            string? accountId = null,
+            string? merchantName = null,
+            string? reference = null,
+            TransactionCurrency? currency = null,
+            TransactionType? type = null,
+            TransactionStatus? status = null);
+    }
+}
---
+++ MvcDemo/Services/ApiClient/TransactionsApiClient.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using TransactionsClient.Services.HttpClientFactory;
+using TransactionsLibrary.Constants;
+using TransactionsLibrary.Models;
+
+namespace MvcDemo.Services.ApiClient
+{
+    public class TransactionsApiClient : ITransactionsApiClient
+    {
+        private readonly IAuthenticatedHttpClientFactory _clientFactory;
+        private readonly ILogger<TransactionsApiClient> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly string _accessToken;
+        public TransactionsApiClient(
+            IAuthenticatedHttpClientFactory clientFactory,
+            IConfiguration configuration,
+            ILogger<TransactionsApiClient> logger,
+            string accessToken
+            )
+        {
+            _clientFactory = clientFactory;
+            _logger = logger;
+            _configuration = configuration;
+            _accessToken = accessToken;
+        }
+        public async Task<bool> CheckHealthy()
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(_configuration["TransactionsApi:BaseUrl"] ?? "https://localhost:7001");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = TimeSpan.FromSeconds(1);
+            _logger.LogInformation("Checking API health");
+
+            var response = await client.GetAsync("/api/health");
+            _logger.LogInformation("API Service is {status}", response.IsSuccessStatusCode ? "healthy" : "unhealthy");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<IReadOnlyList<Transaction>> GetTransactionsAsync(
+            string? accountId = null,
+            string? merchantName = null,
+            string? reference = null,
+            TransactionCurrency? currency = null,
+            TransactionType? type = null,
+            TransactionStatus? status = null)
+        {
+            var client = await _clientFactory.CreateClientAsync(_accessToken);
+            _logger.LogInformation("Fetching available transactions");
+
+            var response = await client.GetAsync($"/api/v1/Transaction/transactions");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var transactions = JsonSerializer.Deserialize<List<Transaction>>(content,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return transactions ?? new List<Transaction>();
+        }
+    }
+}
